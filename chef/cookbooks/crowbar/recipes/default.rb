1fa6bc311a018d0bca0cea8540c0e77798b93c6c
Maximilian Meister <mmeister@suse.de>
2015-08-25 14:13:48 +0200

    Revert "Revert "Listen only on localhost with rainbows""
    This reverts commit 4a50d3a3b4ab788df304c14ef0fad8bf3ae64590.
    
    Conflicts:
    	chef/cookbooks/crowbar/recipes/default.rb

--- chef/cookbooks/crowbar/recipes/default.rb
+++ chef/cookbooks/crowbar/recipes/default.rb
@@ -277,14 +277,14 @@ template "/opt/dell/crowbar_framework/rainbows.cfg" do
   owner "crowbar"
   group "crowbar"
   mode "0644"
-  variables(web_host: "0.0.0.0",
-            web_port: node["crowbar"]["web_port"] || 3000,
-            user: "crowbar",
-            concurrency_model: "EventMachine",
-            group: "crowbar",
-            logdir: logdir,
-            logname: "production",
-            app_location: "/opt/dell/crowbar_framework")
+  variables(:web_host => "127.0.0.1",
+            :web_port => node["crowbar"]["web_port"] || 3000,
+            :user => "crowbar",
+            :concurrency_model => "EventMachine",
+            :group => "crowbar",
+            :logdir => logdir,
+            :logname => "production",
+            :app_location => "/opt/dell/crowbar_framework")
 end
 
 template "/opt/dell/crowbar_framework/rainbows-dev.cfg" do
