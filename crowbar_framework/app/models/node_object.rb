e22016093d182e6b2d27c15f3ac9e876cc2f0b2d
Maximilian Meister <mmeister@suse.de>
2015-09-03 07:57:20 +0200

    adding a class method to determine the admin node

--- crowbar_framework/app/models/node_object.rb
+++ crowbar_framework/app/models/node_object.rb
@@ -170,6 +170,10 @@ class NodeObject < ChefObject
     self.find nil
   end
 
+  def self.admin_node
+    all.select { |n| n if n.admin? }.first
+  end
+
   def self.make_role_name(name)
     "crowbar-#{name.gsub(".", "_")}"
   end
