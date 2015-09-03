48ed8a02bfd5e49a70be3826c5f49fb176c95aa7
Maximilian Meister <mmeister@suse.de>
2015-09-03 08:51:12 +0200

    provide a repository view and json GET API for repository checks
    * new route /utils/repositories
    * new view with a table of repositories and their sanity checks

---
+++ crowbar_framework/app/controllers/repositories_controller.rb
@@ -0,0 +1,37 @@
+#
+# Copyright 2015, SUSE LINUX GmbH
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+class RepositoriesController < ApplicationController
+  before_filter :load_repository_registry
+  #
+  # Repository Check
+  #
+  # Provides the restful api call for
+  # Repository Checks 	/utils/repositories 	GET 	Returns a json list of checked repositories
+  # Renders an HTML view in the UI
+  def index
+    @repocheck = Crowbar::Repository.check_all_repos
+    respond_to do |format|
+      format.html { @repocheck }
+      format.xml { render xml: @repocheck }
+      format.json { render json: @repocheck }
+    end
+  end
+
+  def load_repository_registry
+    Crowbar::Repository.load!
+  end
+end
