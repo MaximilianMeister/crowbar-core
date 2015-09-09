eccb9daa7605f82e85716bb52ff326c86d785f5c
Maximilian Meister <mmeister@suse.de>
2015-09-09 16:39:22 +0200

    adapt the library to be useful for chef
    This library will be used by Rails and Chef, and some methods need a
    different codepath in Rails and in Chef

--- crowbar_framework/lib/crowbar/repository.rb
+++ crowbar_framework/lib/crowbar/repository.rb
@@ -25,7 +25,7 @@ module Crowbar
       # class methods
       #
       def load!
-        @config = YAML.load_file(File.join(Rails.root, "config", "repos.yml"))
+        @config = YAML.load_file(File.expand_path("../../config/repos.yml", File.dirname(__FILE__)))
       end
 
       def registry
@@ -65,15 +65,45 @@ module Crowbar
       end
 
       def admin_platform
-        NodeObject.admin_node.target_platform
+        if in_rails?
+          NodeObject.admin_node.target_platform
+        else
+          provisioner_node["provisioner"]["default_os"]
+        end
       end
 
       def web_port
-        Proposal.where(barclamp: "provisioner").first.raw_attributes["web_port"]
+        if in_rails?
+          Proposal.where(barclamp: "provisioner").first.raw_attributes["web_port"]
+        else
+          provisioner_node["provisioner"]["web_port"]
+        end
       end
 
       def repositories(platform)
         registry[platform]["repos"].keys
+      end
+
+      # This is needed because this library is used by Rails and the Chef provisioner cookbook
+      def in_rails?
+        !defined?(Rails).nil?
+      end
+
+      def admin_ip
+        if in_rails?
+          NodeObject.admin_node.ip
+        else
+          provisioner_node_address
+        end
+      end
+
+      # Chef only methods
+      def provisioner_node
+        Chef::Search::Query.new.search(:node, "roles:provisioner-server").first.first
+      end
+
+      def provisioner_node_address
+        Chef::Recipe::Barclamp::Inventory.get_network_by_type(provisioner_node, "admin").address
       end
     end
 
@@ -116,7 +146,7 @@ module Crowbar
 
     def url
       @registry["url"] || \
-        "http://#{NodeObject.admin_node.ip}:#{Repository.web_port}/#{@platform}/repos/#{@registry["name"]}"
+        "http://#{Repository.admin_ip}:#{Repository.web_port}/#{@platform}/repos/#{@registry["name"]}"
     end
 
     private
